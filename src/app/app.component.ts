06c67a1b2b186376a12c2ef1d253eeb4d15824e0
Austin Laird <alaird@ceiamerica.com>
2019-06-04 20:27:30 -0400

    initial chess game start button and legal moves

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Chess from '../../node_modules/chess.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'chess-app';
+  chessGame: any;
+  moves: any;
+  legalMoves: any;
+
+  startGame(): void {
+    alert('start game');
+    this.chessGame = new Chess();
+
+    this.legalMoves = this.chessGame.moves();
+
+    // while (!this.chessGame.game_over()) {
+    //   var moves = this.chessGame.moves();
+    //   var move = moves[Math.floor(Math.random() * moves.length)];
+    //   this.chessGame.move(move);
+    // }
+  }
 }
